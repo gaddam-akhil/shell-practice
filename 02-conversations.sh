6c13080c29c76da54f5b5044d7a0c19e5f91ddff
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-30 20:24:07 +0530

    con

---
+++ 02-conversations.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "suresh :: hello ramesh,how are you?"
+
+echo "ramesh :: hi suresh, iam fine thanks for asking, how are doing?"
+
+echo "suresh :: doing fine. what are you learning ramesh?"
+
+echo "ramesh :: iam learnig shell. what about you?"
+
