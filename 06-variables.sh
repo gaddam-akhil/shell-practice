fc2378cf38b574a2f4d2db9cf9fbfa7f8e4ea48f
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-30 22:28:39 +0530

    vari

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-START_TIME=$(date +%S)
 
-echo "script executed at $START_TIME"
+
+START_TIME=$(date +%s)
+
+echo "Script started at $START_TIME"
 
 sleep 10
-END_TIME=$(date +%S)
-TOTALTIME=$(($END_TIME-$START_TIME))
 
+END_TIME=$(date +%s)
 
-echo "script executed at:: $TOTALTIME secounds"
+TOTALTIME=$((END_TIME - START_TIME))
+
+echo "Script executed in $TOTALTIME seconds"
+
+#START_TIME=$(date +%S)
+#echo "script executed at $START_TIME"
+#sleep 10
+#END_TIME=$(date +%S)
+#TOTALTIME=$(($END_TIME-$START_TIME))
+#echo "script executed at:: $TOTALTIME secounds"
