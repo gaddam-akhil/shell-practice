a2c562dfc29dda5bffa2b3ac816214ccc9feac22
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-30 20:55:14 +0530

    var

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+PERSON1=suresh
+PERSON2=ramesh
+
+echo "$PERSON1 :: hello $PERSON2,how are you?"
+
+echo "$PERSON2 :: hi suresh, iam fine thanks for asking, how are doing?"
+
+echo "$PERSON1 :: doing fine. what are you learning $PERSON2?"
+
+echo "$PERSON2 :: iam learnig shell. what about you?"
