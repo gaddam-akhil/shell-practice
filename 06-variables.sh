3db55cb1af74fc4dc5e9516ae6b11380611b830e
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-30 22:43:43 +0530

    vari

--- 06-variables.sh
+++ 06-variables.sh
@@ -14,9 +14,9 @@ TOTALTIME=$((END_TIME - START_TIME))
 
 echo "Script executed in $TOTALTIME seconds"
 
-#START_TIME=$(date +%S)
+#START_TIME=$(date +%s)
 #echo "script executed at $START_TIME"
 #sleep 10
-#END_TIME=$(date +%S)
+#END_TIME=$(date +%s)
 #TOTALTIME=$(($END_TIME-$START_TIME))
 #echo "script executed at:: $TOTALTIME secounds"
---
+++ 07-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "all args passed to script:: $@"
+echo "number of args passed to script:: $#"
+echo "script name:: $0"
+echo "present which directory:: $PWD"
+echo "who is runningnthis script:: $USER"
+echo "home directory of the user:: $HOME"
+echo "pid of the script:: $$"
+sleep 20&
+echo "backgroud process id:: $!"
+echo "all args passed to script:: $*"
