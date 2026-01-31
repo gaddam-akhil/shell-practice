de00c46a6070cbbb1fe3400c78a013a2d5f5e208
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 12:00:32 +0530

    mm

--- 05-variables.sh
+++ 05-variables.sh
@@ -12,5 +12,8 @@
 # echo "password is $PASSWORD"
 
 echo "please enter username::"
-read -s USERNAME
-echo "username is - $USERNAME"
+read  USERNAME
+echo "username is :- $USERNAME"
+echo "please enter paswword::"
+read PASSSWORD
+echo "paswword is :- $PASSWORD"
