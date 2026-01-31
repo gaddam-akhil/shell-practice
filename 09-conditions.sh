ba4649c0ab8fc01860712125eb8ff712773c6e8f
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 09:21:07 +0530

    me

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -16,6 +16,8 @@ if [ $NUMBER -gt 20 ]; then
  echo "given number: $NUMBER is greter than 20"
 elif [ $NUMBER -eq 20 ]; then 
  echo "given numer: $NUMBER is equal to 20"
+ elif [ $NUMBER -ne 20 ]
+  echo "given number is: $NUMBER is not equal to 20"
 else
  echo "given $NUMBER: is less than to 20"
  fi
