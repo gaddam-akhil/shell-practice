7a61aec98dc8576733b7fd0d178cfba03c2479e7
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 08:26:24 +0530

    me

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -9,3 +9,7 @@ echo "sum is:: $SUM"
 
 FRUITS=("mango" "Apple" "banana" "Pomo")
 echo "list of friuts:: ${FRUITS[@]}"
+echo "first fruit:: ${FRUITS[0]}"
+echo "secound friut:: ${FRUITS[1]}"
+echo "third fruit:: ${FRUITS[2]}"
+echo "fourth fruit:: ${FRUITS[3]}"
