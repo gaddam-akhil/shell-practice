38c74110aaf36ac51ddcb67e475611c2ffdaed9d
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 23:37:43 +0530

    mm

--- 21-shell.sh
+++ 21-shell.sh
@@ -9,10 +9,28 @@ echo "installing nginx"
 dnf install nginx -y
 
 if [ $? -ne 0 ]; then
-
-echo "installing nginx....FAILURE"
-exit 1
+ echo "installing nginx....FAILURE"
+ exit 1
 else
+ echo "installing nginx...SUCCESS"
+fi
 
-echo "installing nginx...SUCCESS"
+echo "installing mysql"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+ echo "installing mysql....FAILURE"
+ exit 1
+else
+ echo "installing mysql...SUCCESS"
+fi
+
+echo "installing nodejs"
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+ echo "installing nodejs....FAILURE"
+ exit 1
+else
+ echo "installing nodejs...SUCCESS"
 fi
