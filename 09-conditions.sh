11a1df1ad8ab8d470d136343eba2211469987b2d
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 09:18:23 +0530

    me

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,9 +5,17 @@
 #   echo "given number: $NUMBER is greater than 20"
 # fi
 
+# NUMBER=$1
+# if [ $NUMBER -gt 20 ]; then
+#    echo "given number : $NUMBER is greater than 20"
+#  else
+#    echo "given number : $NUMBER is less than 20"
+#  fi
 NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
-   echo "given number : $NUMBER is greater than 20"
- else
-   echo "given number : $NUMBER is less than 20"
+ echo "given number: $NUMBER is greter than 20"
+elif [ $NUMBER -eq 20 ]; then 
+ echo "given numer: $NUMBER is equal to 20"
+else
+ echo "given $NUMBER: is less than to 20"
  fi
