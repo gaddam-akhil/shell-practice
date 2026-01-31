d09a8d61eb9de67a0dd3cba8b19ee6c79f871289
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 12:19:41 +0530

    mm

---
+++ 11-conargsread.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PERSON1=akhil
+PERSON2=$1
+PERSON3=$2
+
+echo "$PERSON2 :: hi $PERSON1, do yo have online cash"
+echo "$PERSON1 :: hello $PERSON2, I HAVE"
+echo "$PERSON3 :: I ALSO NEED CAN YOU SEND ME ALSO $PERSON1"
+echo "$PERSON1 :: ok"
+echo "$PERSON2 :: GIVE YOUR PHONE WE WILL SEND monEY $PERSON1"
+echo "$PERSON3 :: aks to $PERSON1"
+echo "enter your username ::"
+read USERNAME
+echo "username is $USERNAME"
+echo "$PERSON2 ask password also"
+echo "enter you password ::"
+read -s PASSWORD
+echo "password is $PASSWORD"
