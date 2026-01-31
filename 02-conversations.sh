0289cf0ea96c61085ce6fb214820326de8a9fab9
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 11:39:33 +0530

    mm

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -7,11 +7,22 @@
 # echo "suresh :: doing fine. what are you learning ramesh?"
 
 # echo "ramesh :: iam learnig shell. what about you?"
-PERSON1=laddu
-PERSON2=sunny
+# PERSON1=laddu
+# PERSON2=sunny
 
-echo "$PERSON1:: hi $PERSON2, how are you?"
-echo "$PERSON2:: hi $PERSON1, am doing good, how are you?"
-echo "$PERSON1:: am fine thanks for asking, what are you learling $PERSON2?"
-echo "$PERSON2:: am learning shell"
+# echo "$PERSON1:: hi $PERSON2, how are you?"
+# echo "$PERSON2:: hi $PERSON1, am doing good, how are you?"
+# echo "$PERSON1:: am fine thanks for asking, what are you learling $PERSON2?"
+# echo "$PERSON2:: am learning shell"
 
+PERSON1=AKHIL
+PERSON2=ARUN
+
+echo "$PERSON1: Hey $PERSON2, how are you?"
+echo "$PERSON2: Im good, $PERSON1. What about you?"
+echo "$PERSON1: Im fine too. What are you doing these days?"
+echo "$PERSON2: Im learning shell scripting. Its a bit hard."
+echo "$PERSON1: Dont worry. Practice daily and you will improve."
+echo "$PERSON2: Yes, I practice every day. Thanks for the support."
+echo "$PERSON1: Anytime! Lets have tea later."
+echo "$PERSON2: Sure, see you in the evening"
