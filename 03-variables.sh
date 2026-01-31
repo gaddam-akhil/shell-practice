243c98b1e791bd9a19642ef1fa47c3302d22909d
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 11:53:59 +0530

    mm

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,18 @@
 #!/bin/bash 
 
-PERSON1=putin
-PERSON2=trump
+# PERSON1=putin
+# PERSON2=trump
+
+# echo "$PERSON1 :: hello $PERSON2,how are you?"
+
+# echo "$PERSON2 :: hi suresh, iam fine thanks for asking, how are doing?"
+
+# echo "$PERSON1 :: doing fine. what are you learning $PERSON2?"
+
+# echo "$PERSON2 :: iam learnig shell. what about you?"
+
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1 :: hello $PERSON2,how are you?"
 
