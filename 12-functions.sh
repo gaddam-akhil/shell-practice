1107a1cb3182a9b0cc13758666a71d2708a902c0
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 17:16:02 +0530

    mm

--- 12-functions.sh
+++ 12-functions.sh
@@ -2,21 +2,15 @@
 
 if [ $USER_ID -ne 0 ]; then
  echo "please run this script as root user access" 
-
  exit 1
  fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-
    echo "$2 .... FAILURE"
-
-   exit 1
-
+ exit 1
  else 
-
    echo "$2 .. nginx SUCCESS"
-
  fi
 }
  
@@ -24,6 +18,10 @@ VALIDATE() $? "installing nginx"
 
  dnf install nginx -y
 
+VALIDATE() $? "installing mysql"
+
  dnf install mysql -y
+
+VALIDATE() $? "installing nodejs"
 
  dnf install nodejs -y
