38aaa5b394b0d299c47b671e03f8d18e9dfcbdc6
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 12:26:09 +0530

    mm

--- 06-variables.sh
+++ 06-variables.sh
@@ -2,17 +2,17 @@
 
 
 
-START_TIME=$(date +%s)
+# START_TIME=$(date +%s)
 
-echo "Script started at $START_TIME"
+# echo "Script started at $START_TIME"
 
-sleep 10
+# sleep 10
 
-END_TIME=$(date +%s)
+# END_TIME=$(date +%s)
 
-TOTALTIME=$((END_TIME - START_TIME))
+# TOTALTIME=$((END_TIME - START_TIME))
 
-echo "Script executed in $TOTALTIME seconds"
+# echo "Script executed in $TOTALTIME seconds"
 
 #START_TIME=$(date +%s)
 #echo "script executed at $START_TIME"
@@ -20,3 +20,12 @@ echo "Script executed in $TOTALTIME seconds"
 #END_TIME=$(date +%s)
 #TOTALTIME=$(($END_TIME-$START_TIME))
 #echo "script executed at:: $TOTALTIME secounds"
+#I want a command to be executed and take the output into variable,how to do that
+START_TIME=(date +%s)
+echo "start time is - $START_TIME"
+END_TIME=(date +%s)
+echo "end time is -$END_TIME"
+
+TOTALTIME=$(($START_TIME-$END_TIME))
+
+echo "TOTALTIME is $TOTALTIME"
