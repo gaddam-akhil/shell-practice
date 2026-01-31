d130bc52290525fe601945299c4257a12d19b10e
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-01 00:23:02 +0530

    mm

--- 21-shell.sh
+++ 21-shell.sh
@@ -5,16 +5,16 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILES="/var/log/shell-script/$0.log"
 
  if [ $USER_ID -ne 0 ]; then
-     echo "please run the script with root user access"
+     echo "please run the script with root user access" | tee -a $LOGS_FILES
      exit 1
 fi
      mkdir -p $LOGS_FOLDER
      VALIDATE(){
 if [ $1 -ne 0 ]; then
-    echo "$2....FAILURE"
+    echo "$2....FAILURE" | tee -a $LOGS_FILES
     exit 1
 else
-    echo "$2...SUCCESS"
+    echo "$2...SUCCESS" | tee -a $LOGS_FILES
 fi
 }
 
