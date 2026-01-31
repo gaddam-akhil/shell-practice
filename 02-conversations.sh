701dcbec0fc590d90961195a470006468f872ac2
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 11:43:33 +0530

    mm

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -17,12 +17,15 @@
 
 PERSON1=$1
 PERSON2=$2
+PERSON3=$3
+PERSON4=$4
 
 echo "$PERSON1: Hey $PERSON2, how are you?"
 echo "$PERSON2: Im good, $PERSON1. What about you?"
 echo "$PERSON1: Im fine too. What are you doing these days?"
-echo "$PERSON2: Im learning shell scripting. Its a bit hard."
-echo "$PERSON1: Dont worry. Practice daily and you will improve."
-echo "$PERSON2: Yes, I practice every day. Thanks for the support."
+echo "$PERSON2: Im learning shell scripting. $PERSON3 a bit hard."
+echo "$PERSON3: Dont worry. Practice daily and you will improve."
+echo "$PERSON2: Yes, I practice $PERSON4 every day. Thanks for the support."
 echo "$PERSON1: Anytime! Lets have tea later."
-echo "$PERSON2: Sure, see you in the evening"
+echo "$PERSON4: Sure, see you in the evening"
+echo "$PERSON1 = $PERSON2 = $PERSON3 = $PERSON4"
