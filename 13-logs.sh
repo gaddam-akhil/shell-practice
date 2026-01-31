83e86ba18b9b60e2d2b85f629abfe06c3f7339e7
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 18:11:01 +0530

    mm

---
+++ 13-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILES="/var/log/shell-script/$0.log"
+
+if [ $USER_ID -ne 0 ]; then
+ echo "please run this script as root user access" 
+exit 1
+ fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+   echo "$2 .... FAILURE"
+exit 1
+ else 
+   echo "$2 .. nginx SUCCESS"
+ fi
+}
+
+dnf install nginx -y &>>$LOGS_FILES
+VALIDATE $? "installing nginx"
+
+dnf install mysql -y &>>$LOGS_FILES
+VALIDATE $? "installing mysql"
+
+dnf install nodejs -y &>>$LOGS_FILES
+VALIDATE $? "installing nodejs" 
