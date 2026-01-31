6d3a4c22fbc499571d9f8ab30d0ca33c15992aed
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-01 00:41:06 +0530

    mm

--- 21-shell.sh
+++ 21-shell.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR 
 
 USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
@@ -13,14 +15,14 @@ N="\e[0m"
      exit 1
 fi
      mkdir -p $LOGS_FOLDER
-     VALIDATE(){
-if [ $1 -ne 0 ]; then
-    echo -e "$2....$R FAILURE $N" | tee -a $LOGS_FILES
-    exit 1
-else
-    echo -e "$2...$G SUCCESS $N" | tee -a $LOGS_FILES
-fi
-}
+#      VALIDATE(){
+# if [ $1 -ne 0 ]; then
+#     echo -e "$2....$R FAILURE $N" | tee -a $LOGS_FILES
+#     exit 1
+# else
+#     echo -e "$2...$G SUCCESS $N" | tee -a $LOGS_FILES
+# fi
+# }
 
 # dnf install nginx -y &>> $LOGS_FILES 
 # VALIDATE $? "installing nginx"
@@ -36,7 +38,7 @@ do
   dnf install $PACKAGE -y &>> $LOGS_FILES
 if [ $? -ne 0 ]; then
   echo "$PACKAGE is not installed, installing now"
-  VALIDATE $? "installing $PACKAGE" 
+  #VALIDATE $? "installing $PACKAGE" 
 else
   echo -e "$PACKAGE already installed....$Y skipping $N"
   fi
