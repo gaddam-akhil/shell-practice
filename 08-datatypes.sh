b10f34c940e45a5e4b9cdd1ef455956e8c72c055
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 13:18:15 +0530

    mm

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# NUM1=100
+# NUM2=akhil 
+
+# SUM=$(($NUM1+$NUM2))
+# echo "sum is:: $SUM"
+
+
+# FRUITS=("mango" "Apple" "banana" "Pomo")
+# echo "list of friuts:: ${FRUITS[@]}"
+# echo "first fruit:: ${FRUITS[0]}"
+# echo "secound friut:: ${FRUITS[1]}"
+# echo "third fruit:: ${FRUITS[2]}"
+# echo "fourth fruit:: ${FRUITS[3]}"
+
 NUM1=100
-NUM2=akhil 
+NUM2=200
 
 SUM=$(($NUM1+$NUM2))
-echo "sum is:: $SUM"
-
-
-FRUITS=("mango" "Apple" "banana" "Pomo")
-echo "list of friuts:: ${FRUITS[@]}"
-echo "first fruit:: ${FRUITS[0]}"
-echo "secound friut:: ${FRUITS[1]}"
-echo "third fruit:: ${FRUITS[2]}"
-echo "fourth fruit:: ${FRUITS[3]}"
+echo "sum is $SUM
