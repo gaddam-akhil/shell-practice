01cd6b3b5660e537ee2f62a612627f54bf4d3490
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 11:28:17 +0530

    mm

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-echo "suresh :: hello ramesh,how are you?"
+# echo "suresh :: hello ramesh,how are you?"
 
-echo "ramesh :: hi suresh, iam fine thanks for asking, how are doing?"
+# echo "ramesh :: hi suresh, iam fine thanks for asking, how are doing?"
 
-echo "suresh :: doing fine. what are you learning ramesh?"
+# echo "suresh :: doing fine. what are you learning ramesh?"
 
-echo "ramesh :: iam learnig shell. what about you?"
+# echo "ramesh :: iam learnig shell. what about you?"
+
+echo "akhil:: hi arun, how are you?"
+echo "arun:: hi akhil, am doing good, how are you?"
+echo "akhil:: am fine thanks for asking, what are you learling arun?"
+esho "arun:: am learning shell"
 
