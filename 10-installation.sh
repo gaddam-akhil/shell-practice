f324cb6c79bcbdd088cc262f5d8013d303f0acfb
gaddam akhil <gaddamakhil900@gmail.com>
2026-01-31 15:20:28 +0530

    mm

--- 10-installation.sh
+++ 10-installation.sh
@@ -37,31 +37,35 @@
 
 #   fi
 
+# USER_ID=$(id -u)
+# echo "user id is $USER_ID"
+# ROOT_ID=$(sudo id -u)
+# echo "root id is $ROOT_ID"
+
+# if [ $USER_ID -ne 0 ]; then
+#  echo "please run this script as root user access"
+#  exit 1
+#  fi
+
+#  echo "installing nginx"
+
+#  dnf install nginx -y
+# echo "$?"
+# if [ $? -ne 0 ]; then
+
+#  echo "installing nginx FAILURE"
+
+#  exit 1
+
+#  else 
+
+#  echo "installing nginx SUCCESS"
+
+#  fi
+
+
 USER_ID=$(id -u)
 echo "user id is $USER_ID"
-ROOT_ID=$(sudo id -u)
-echo "root id is $ROOT_ID"
-
-if [ $USER_ID -ne 0 ]; then
- echo "please run this script as root user access"
- exit 1
- fi
-
- echo "installing nginx"
-
- dnf install nginx -y
-echo "$?"
-if [ $? -ne 0 ]; then
-
- echo "installing nginx FAILURE"
-
- exit 1
-
- else 
-
- echo "installing nginx SUCCESS"
-
- fi
 
 
 
