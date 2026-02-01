788f2ecc21e42c4d58611e2d7b582dbe9d7d1a4f
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-01 11:15:44 +0530

    mm

--- 22-shell.sh
+++ 22-shell.sh
@@ -4,25 +4,25 @@ USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILES="/va/log/shell-script/$0.log"
  if [ $USER_ID -ne 0 ]; then
-   echo " please run this script as root user access" &>>$0.log
+   echo " please run this script as root user access" &>>$LOGS_FILES
    exit 1
  fi  
   mkdir -p $LOGS_FOLDER
 
  VALIDATE(){
  if [ $1 -ne 0 ]; then
-   echo "$2  FAILURE"
+   echo "$2  FAILURE" &>>$0LOGS_FILES
    exit 1
 else
-   echo " $2 SuCCESS"
+   echo " $2 SuCCESS" &>>$0LOGS_FILES
  fi
  }
 
 dnf install nginx -y
-VALIDATE $? "installing nginx" &>>$0.log
+VALIDATE $? "installing nginx" &>>$0LOGS_FILES
 
 dnf install mysql -y
-VALIDATE $? "installing mysql" &>>$0.log
+VALIDATE $? "installing mysql" &>>$LOGS_FILES
 
 dnf install nodejs -y
-VALIDATE $? "installing nodejs" &>>$0.log
+VALIDATE $? "installing nodejs" &>>$LOGS_FILES
