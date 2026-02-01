ffa853905eaaaaec1ddee503a1f126665a50c531
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-01 11:33:32 +0530

    mm

--- 22-shell.sh
+++ 22-shell.sh
@@ -3,23 +3,33 @@
 USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILES="/var/log/shell-script/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
  if [ $USER_ID -ne 0 ]; then
-   echo " please run this script as root user access" | tee -a $LOGS_FILES
+   echo -e " please run this script as root user access" | tee -a $LOGS_FILES
    exit 1
  fi  
   mkdir -p $LOGS_FOLDER
 
  VALIDATE(){
  if [ $1 -ne 0 ]; then
-   echo "$2  FAILURE" | tee -a $LOGS_FILES
+   echo -e "$2 $R FAILURE $N" | tee -a $LOGS_FILES
    exit 1
 else
-   echo " $2 SuCCESS" | tee -a $LOGS_FILES
+   echo -e " $2 $G SuCCESS $N" | tee -a $LOGS_FILES
  fi
  } 
 
 for PACKAGE in $@
  do 
- dnf install $PACKAGE -y  &>> $LOGS_FILES 
- VALIDATE $? "installing $PACKAGE"
+  dnf install $PACKAGE -y  &>> $LOGS_FILES 
+ if [ $? -ne 0 ]
+  echo "installing $PACKAGE"
+ else
+  VALIDATE $? "installing $PACKAGE"
+  echo -e "already installed $PACKAGE...$Y skipping $N"
+ 
  done 
