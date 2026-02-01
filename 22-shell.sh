c88b37128de538d924ec8596874e54162b826b2d
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-01 10:35:52 +0530

    mm

--- 22-shell.sh
+++ 22-shell.sh
@@ -3,7 +3,27 @@
 USER_ID=$(id -u)
  if [ $USER_ID -ne 0 ]; then
    echo " please run this script as root user access"
- exit 1
+   exit 1
  fi  
-echo "installing nginx"
-dnf install nginx -y
+   echo "installing nginx"
+   dnf install nginx -y
+ if [ $? -ne 0 ]; then
+   echo "installing nginx FAILURE"
+ else 
+   exit 1
+   echo "installing nginx SuCCESS"
+ fi
+   dnf install mysql -y
+ if [ $? -ne 0 ]; then
+   echo "installing mysql FAILURE"
+ else 
+   exit 1
+   echo "installing mysql SuCCESS"
+ fi
+   dnf install nginx -y
+ if [ $? -ne 0 ]; then
+   echo "installing nodejs FAILURE"
+ else 
+   exit 1
+   echo "installing nodejs SuCCESS"
+ fi
