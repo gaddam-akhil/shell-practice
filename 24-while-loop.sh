95d6287638bb7b724f143c48ea82a5f1707e3bad
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-05 00:59:17 +0530

    common

--- 24-while-loop.sh
+++ 24-while-loop.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Initialize a counter variable
+# # Initialize a counter variable
 count=1
 
-# Loop as long as 'count' is less than or equal to 5
-while [ $count -le 5 ]
+# # Loop as long as 'count' is less than or equal to 5
+# while [ $count -le 5 ]
+# do
+#     echo "Count is $count"
+#     sleep 1
+#     # Increment the counter
+#     ((count++))
+# done
+
+# Open the file for reading and redirect its content to the while loop
+while IFS= read -r line;
 do
-    echo "Count is $count"
-    sleep 1
-    # Increment the counter
-    ((count++))
-done
+  # Process the each line (here, we just print it)
+  echo "$line"
+done < $"14-loops.sh #input which file to read
