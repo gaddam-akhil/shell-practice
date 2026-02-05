011a16b0984547d71e6592628888714882050d16
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-05 22:47:51 +0530

    logs del backup

--- 25-backup.sh
+++ 25-backup.sh
@@ -59,7 +59,7 @@ fi
     TIMESTAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
     echo "archieve name : $ZIP_FILE_NAME"
-    find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS | tar -zcvf $ZIP_FILE_NAME
+    tar -zcvf $ZIP_FILE_NAME (find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 #check archieve is succes or not
 if [ -f $ZIP_FILE_NAME ]; then
    log "archeival is ... $G success $N"
