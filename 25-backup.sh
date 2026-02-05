ec9d4897b0adb9cf01ba9dcac1fbe465f3067e9e
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-05 20:44:34 +0530

    common

--- 25-backup.sh
+++ 25-backup.sh
@@ -7,7 +7,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+SOURCE_DIR=$1
+DEST_DIR=$1
+DAYS=$(3:14)
 
  if [ $USER_ID -ne 0 ]; then
      echo -e "$R please run the script with root user access $N" 
@@ -21,6 +23,31 @@ fi
        exit 1
    }
 
+   log(){
+     echo -e "$G $(date "+%y-%m-%d %H:%M:%S") | $1 $N" | tee -a $LOGS_FILES
+   }
+
+
    if [ $# -lt 2 ]; then
      USAGE
    fi
+
+   if [ ! -d $SOURCE_DIR ]; then
+      echo -e "$R SOURCE DIRECTORY :: $SOURCE_DIR does not exist $N"
+      exit 1   
+   fi
+
+   if [ ! -d $DEST_DIR ]; then
+      echo -e "$R DEST DIRECTORY :: $DEST_DIR does not exist $N"
+    
+      exit 1
+   fi
+
+#find the files
+  FILES=$(find $SOURCE_DIR -name "*.log" -type -f +$DAYS)
+
+  log "backup started"
+  log "source directory : $SOURCE_DIR"
+  log "destination directory : $DEST_DIR"
+  log "Days : $DAYS"
+
