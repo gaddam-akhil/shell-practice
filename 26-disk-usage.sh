647d1475825b5db7387ade9637d6b30bb5fb45dc
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-06 18:36:56 +0530

    logs del backup

---
+++ 26-disk-usage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+USAGE_THRESHOLD=3
+
+while IFS= read -r line;
+do
+  USAGE=$(echo  $line | awk '{print $6}' | cut -d "%" -f1)
+  PARTITION=$(echo $line | awk '{print &7}')
+
+  if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+   MESSAGE+="High Disk usage on $PRATITION:$USAGE"
+  fi
+  echo "$line"
+done <<< $DISK_USAGE
+
+echo "MESSAGE"
