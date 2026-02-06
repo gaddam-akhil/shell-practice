ddc5ec0fc3ee6108676185e196d46aaaa8483633
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-06 20:41:59 +0530

    logs del backup

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -34,6 +34,29 @@ MESSAGE=""
 #to get ip address of instance
 IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
+# log(){
+#     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
+# }
+
+# DISK_USAGE=$(df -hT | grep -v Filesystem)
+# USAGE_THRESHOLD=3
+
+# while IFS= read -r line
+# do
+#     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+#     PARTITION=$(echo $line | awk '{print $7}')
+
+#     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+#         MESSAGE+="High Disk usage on $PARTITION: $USAGE <br>"
+#     fi
+# done <<< $DISK_USAGE
+
+# echo -e "$MESSAGE"
+
+# sh mail.sh "gaddamakhil37@gmail.com" "high disk usage alert on $IP_ADDRESS"  "$MESSAGE"  "HIGH DISK USAGE"  "$IP_ADDRESS" "DEVOPS TEAM"
+
+# # sh mail.sh "info@joindevops.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
+
 log(){
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
 }
@@ -47,12 +70,10 @@ do
     PARTITION=$(echo $line | awk '{print $7}')
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE+="High Disk usage on $PARTITION: $USAGE <br>"
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"
     fi
 done <<< $DISK_USAGE
 
 echo -e "$MESSAGE"
 
-sh mail.sh "gaddamakhil37@gmail.com" "high disk usage alert on $IP_ADDRESS"  "$MESSAGE"  "HIGH DISK USAGE"  "$IP_ADDRESS" "DEVOPS TEAM"
-
-# sh mail.sh "info@joindevops.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
+sh mail.sh "gaddamakhil37@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
