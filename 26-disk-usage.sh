01b037f93af516fc351268af30cfb7945fb4b96b
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-06 18:45:50 +0530

    logs del backup

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -5,22 +5,49 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+# log(){
+#     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+# # }
+
+# DISK_USAGE=$(df -hT | grep -v Filesystem)
+# USAGE_THRESHOLD=3
+
+# while IFS= read -r line;
+# do
+#   USAGE=$(echo  $line | awk '{print $6}' | cut -d "%" -f1)
+#   PARTITION=$(echo $line | awk '{print &7}')
+
+#   if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+#    MESSAGE+="High Disk usage on $PRATITION:$USAGE"
+#   fi
+#   echo "$line"
+# done <<< $DISK_USAGE
+
+# echo "MESSAGE"
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MESSAGE=""
+# IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
 log(){
-    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
 }
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=3
 
-while IFS= read -r line;
+while IFS= read -r line
 do
-  USAGE=$(echo  $line | awk '{print $6}' | cut -d "%" -f1)
-  PARTITION=$(echo $line | awk '{print &7}')
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
 
-  if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-   MESSAGE+="High Disk usage on $PRATITION:$USAGE"
-  fi
-  echo "$line"
+    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"
+    fi
 done <<< $DISK_USAGE
 
-echo "MESSAGE"
+echo -e "$MESSAGE"
